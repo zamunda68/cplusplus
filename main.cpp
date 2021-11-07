5a61fa8bfe229a83847b234e5e14f3eed7ce95b0
zamunda68 <zamunda68@gmail.com>
2021-11-07 12:09:18 +0200

    Simple calculator commit 1

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string name = "John";
+    int age;
+
+        cout << "Hello " << name << " please, enter your " << age << " here:";
+        cin >> age;
+
+    return 0;
+}
